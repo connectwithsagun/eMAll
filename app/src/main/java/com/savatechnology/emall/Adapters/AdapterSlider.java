538000df18b40a16dceff5ab60d38a6e44e2ff19
unknown <sagunpangeni136@gmail.com>
2021-03-25 15:54:34 +0545

    home activity

---
+++ app/src/main/java/com/savatechnology/emall/Adapters/AdapterSlider.java
@@ -0,0 +1,92 @@
+package com.savatechnology.emall.Adapters;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.bumptech.glide.Glide;
+import com.savatechnology.emall.R;
+import com.smarteist.autoimageslider.SliderViewAdapter;
+import com.savatechnology.emall.Models.SliderImageList;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdapterSlider extends
+        SliderViewAdapter<AdapterSlider.SliderAdapterVH> {
+
+    private Context context;
+    private List<SliderImageList> mSliderItems = new ArrayList<>();
+
+    public AdapterSlider(Context context) {
+        this.context = context;
+    }
+
+    public void renewItems(List<SliderImageList> sliderItems) {
+        this.mSliderItems = sliderItems;
+        notifyDataSetChanged();
+    }
+
+    public void deleteItem(int position) {
+        this.mSliderItems.remove(position);
+        notifyDataSetChanged();
+    }
+
+    public void addItem(SliderImageList sliderItem) {
+        this.mSliderItems.add(sliderItem);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public SliderAdapterVH onCreateViewHolder(ViewGroup parent) {
+        View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_image_slider, null);
+        return new SliderAdapterVH(inflate);
+    }
+
+    @Override
+    public void onBindViewHolder(SliderAdapterVH viewHolder, final int position) {
+
+        SliderImageList sliderItem = mSliderItems.get(position);
+
+        viewHolder.textViewDescription.setText(sliderItem.getDescription());
+        viewHolder.textViewDescription.setTextSize(16);
+        viewHolder.textViewDescription.setTextColor(Color.WHITE);
+        Glide.with(viewHolder.itemView)
+                .load(sliderItem.getImageUrl())
+                .fitCenter()
+                .into(viewHolder.imageViewBackground);
+
+        viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "This is item in position " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getCount() {
+        //slider view count could be dynamic size
+        return mSliderItems.size();
+    }
+
+    class SliderAdapterVH extends SliderViewAdapter.ViewHolder {
+
+        View itemView;
+        ImageView imageViewBackground;
+        ImageView imageGifContainer;
+        TextView textViewDescription;
+
+        public SliderAdapterVH(View itemView) {
+            super(itemView);
+            imageViewBackground = itemView.findViewById(R.id.iv_auto_image_slider);
+            imageGifContainer = itemView.findViewById(R.id.iv_gif_container);
+            textViewDescription = itemView.findViewById(R.id.tv_auto_image_slider);
+            this.itemView = itemView;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/savatechnology/emall/Models/SliderImageList.java
@@ -0,0 +1,23 @@
+package com.savatechnology.emall.Models;
+
+public class SliderImageList {
+
+    private String description;
+    private String imageUrl;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
